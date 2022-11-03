6f79fcd7c207f0a635972860e03cc8bbdd228278
Bhanu Prakash <pebbetibhanu2017@gmail.com>
2022-11-03 19:18:52 +0530

    Add files via upload

--- classifierLite.py
+++ classifierLite.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 import cv2
 import numpy as np
-from RoiCutter import create_model,cut_image
+from RoiCutter import create_model, cut_image_lite
 
 class classification_module:
   def __init__(self,path):
@@ -12,7 +13,11 @@ class classification_module:
     self.output_details = self.interpreter.get_output_details()
     self.input_shape = (self.input_details[0]['shape'][1],self.input_details[0]['shape'][2])
     self.classes = {0:"negative",1:"positive"}
-    self.obj_cut = create_model((self.input_shape[0],self.input_shape[1],3),c=[8,16,32,64])
+    self.interpreter_obj = tf.lite.Interpreter(model_path="/home/pi/app/models/obj_cut_lite.tflite")
+    self.interpreter_obj.allocate_tensors()
+    self.input_details_obj = self.interpreter_obj.get_input_details()
+    self.output_details_obj = self.interpreter_obj.get_output_details()
+    self.input_shape_obj = (self.input_details_obj[0]['shape'][1],self.input_details_obj[0]['shape'][2])
 
   def preprocess(self,image):
     image = cv2.resize(image,(self.input_shape))/255
@@ -20,7 +25,12 @@ class classification_module:
     return image
 
   def apply_roi_cutting(self,image):
-    return cut_image(self.obj_cut,image,self.input_shape[0])
+    image_roi = cv2.resize(image,(self.input_shape))/255
+    image_roi = np.expand_dims(np.array(image_roi, dtype=np.float32), axis=0)
+    self.interpreter_obj.set_tensor(self.input_details_obj[0]['index'], image_roi)
+    self.interpreter_obj.invoke()
+    output_data = self.interpreter_obj.get_tensor(self.output_details_obj[0]['index'])
+    return cut_image_lite(output_data,image,self.input_shape[0])
 
   def classify(self,image):
     self.interpreter.set_tensor(self.input_details[0]['index'], image)
